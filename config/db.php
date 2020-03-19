ee7c348af1610b7bdb40652b6780b49eb288e26e
Abel_Tseng <zfup1105@gmail.com>
2020-03-19 15:39:12 +0800

    增加财务报表
    Signed-off-by: Abel_Tseng <zfup1105@gmail.com>

--- config/db.php
+++ config/db.php
@@ -6,7 +6,7 @@ return array(
     'DB_TYPE' => 'mysql',
     'DB_HOST' => 'localhost',
     'DB_USER' => 'root',
-    'DB_PWD' => 'ifNzwPThAKGo3xTv',
+    'DB_PWD' => '*****',
     'DB_PORT' => 3306,
     'DB_NAME' => 'xc_barbershop',
     'DB_CHARSET' => 'utf8',
@@ -15,27 +15,3 @@ return array(
 
 
 //阿里PolarDB
-/*return [
-    'default' => [
-        // 数据库配置
-        'DB_TYPE' => 'mysql',
-        'DB_HOST' => 'pc-bp199rw2ro8635l02.rwlb.rds.aliyuncs.com', //集群地址
-        'DB_USER' => 'jk_gongxiang',
-        'DB_PWD' => 'P3nuozr#EqYV&HoF9b',
-        'DB_PORT' => 3306,
-        'DB_NAME' => 'jk_gongxiang',
-        'DB_CHARSET' => 'utf8',
-        'DB_PREFIX' => 'mi_',
-    ],
-    'master' => [
-        // 数据库配置
-        'DB_TYPE' => 'mysql',
-        'DB_HOST' => 'pc-bp199rw2ro8635l02.mysql.polardb.rds.aliyuncs.com', //主地址
-        'DB_USER' => 'jk_gongxiang',
-        'DB_PWD' => 'P3nuozr#EqYV&HoF9b',
-        'DB_PORT' => 3306,
-        'DB_NAME' => 'jk_gongxiang',
-        'DB_CHARSET' => 'utf8',
-        'DB_PREFIX' => 'mi_',
-    ]
-];*/
